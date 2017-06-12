d34d39c55b68089769ceb5131fecdb3cf9b1da37
Osama Hassanein <player1os@gmail.com>
2017-06-12 13:02:00 +0200

    Allow for an optional passing of the next parameter

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const nextObject = {
 // Converts a promise into a callback that is acceptable by express.
 // If any of the special objects is returned as the result of the promise the next function is called.
 // Otherwise no following middleware is executed.
-const factory = (promiseGenerator: (req: Request, res: Response, next: { route: object }) => Promise<void | object>) => {
+const factory = (promiseGenerator: (req: Request, res: Response, next?: { route: object }) => Promise<void | object>) => {
 	return (req: Request, res: Response, next: NextFunction) => {
 		Promise.resolve(promiseGenerator(req, res, nextObject))
 			.then((result: object) => {

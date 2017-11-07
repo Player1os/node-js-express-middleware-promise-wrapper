9cee22b7821f99dfa17fea61e91d7a8f608ec4d0
Osama Hassanein <player1os@gmail.com>
2017-11-07 13:43:27 +0100

    Add the error middleware to the library's exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import {
-	HttpServer,
-	THttpServerRequestListener,
-} from '.../src//http_server'
+import errorMiddleware from '.../src/error_middleware'
 import getHostname from '.../src/get_hostname'
 import getIpAddress from '.../src/get_ip_address'
 import getOriginalUrl from '.../src/get_original_url'
+import {
+	HttpServer,
+	THttpServerRequestListener,
+} from '.../src/http_server'
 import {
 	default as promiseWrapper,
 	promisifyResponseFinish,
 } from '.../src/promise_wrapper'
 
 export {
-	HttpServer,
+	errorMiddleware,
 	getHostname,
 	getIpAddress,
 	getOriginalUrl,
+	HttpServer,
 	promisifyResponseFinish,
 	promiseWrapper,
 	THttpServerRequestListener,
